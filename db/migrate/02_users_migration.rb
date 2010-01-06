2998d0f4d17533b0c6039bb52e2da4b19f63fad4
Kyle Maxwell <kyle@kylemaxwell.com>
2010-01-05 20:23:19 -0800

    wip

---
+++ db/migrate/02_users_migration.rb
@@ -0,0 +1,13 @@
+class UsersMigration < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :handle
+      t.text :classifier
+    end  
+    add_index :votes, :handle
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
